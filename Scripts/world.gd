2ff9263e23fedd330339a867b384b963cb78fba8
User <User@LAPTOP-RGL4QFQ2>
2026-01-22 11:07:20 +0800

    trigger added tutorial leve 1,2

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export_file("*.tscn") var start_level_path: String = "res://Scenes/tutorial.tscn"
+@export_file("*.tscn") var start_level_path: String = "res://Scenes/level_1.tscn"
 
 func _ready() -> void:
 	if _has_level_loaded():
