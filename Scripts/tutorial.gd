6ed79282dd7c225e82e6d0f70d955f339fe00fc6
User <User@LAPTOP-RGL4QFQ2>
2026-01-22 10:34:14 +0800

    Tutorial and level 1 trigger point

--- Scripts/tutorial.gd
+++ Scripts/tutorial.gd
@@ -2,3 +2,15 @@ extends Node3D
 
 @export_file("*.tscn") var next_level_path: String = "res://Scenes/level_1.tscn"
 	
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_body_entered2(body: Node3D) -> void:
+	pass # Replace with function body.
