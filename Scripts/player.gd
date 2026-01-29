746fcb2707eec4c6f04c028528b6d24b7822a473
John Kyle Villarazo <villarazojk@gmail.com>
2026-01-29 20:48:45 +0800

    added level_4 ambot ani hahays

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -47,6 +47,8 @@ var current_battery : float = 5.0
 var is_nv_on : bool = false
 var recharge_speed : float = 0.5
 
+var push_force : float = 2.0
+
 func _ready() -> void:
 	GameSettings.load_settings()
 	nv_layer.visible = false
@@ -65,6 +67,9 @@ func _ready() -> void:
 func activate_sprint():
 	print("Sprint Activated")
 	can_sprint = true
+	
+	crosshair.show_tutorial_warning("⚠ ALARM TRIGGERED! FIND EXIT\n[HOLD SHIFT] TO RUN", 7.0)
+	
 	
 func _unhandled_input(event: InputEvent):
 	if is_cutscene and event is InputEventMouseMotion:
@@ -111,8 +116,25 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 	
+	_handle_pushing_rigid_bodies()
+	
 	_handle_fov(delta)
 	
+	
+func _handle_pushing_rigid_bodies():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		
+		if collider is RigidBody3D:
+			var push_dir = -collision.get_normal()
+			push_dir.y = 0 
+			push_dir = push_dir.normalized()
+			
+			var velocity_diff = velocity.dot(push_dir)
+			
+			if velocity_diff > 0:
+				collider.apply_central_impulse(push_dir * push_force * collider.mass * 0.1)
 	
 func _handle_grabbing_and_interacting():
 	if holding_object and held_object != null:
@@ -297,19 +319,15 @@ func _try_load_next_level():
 		print("No next level found. Respawning in current level.")
 		respawn()
 
-# Level Swap
 func _perform_level_swap(world_node, old_level_node, new_level_path):
-	# Load the new level
 	var new_level_resource = load(new_level_path)
 	var new_level_instance = new_level_resource.instantiate()
 	
-	# Add it to the world
 	world_node.add_child(new_level_instance)
 	
-	# Delete the old level
 	old_level_node.queue_free()
 	
-	respawn()
+	respawn()	
 	
 	print("Level Swapped Successfully!")
 	
